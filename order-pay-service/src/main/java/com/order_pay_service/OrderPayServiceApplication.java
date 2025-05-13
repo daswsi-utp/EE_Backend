20c6cde411194048d61865e5fc13941217b6a000
Marlon Diego <105674967+MarlonLupaca@users.noreply.github.com>
2025-05-13 11:49:02 -0500

    chore: create initial folder structure for microservice

---
+++ order-pay-service/src/main/java/com/order_pay_service/OrderPayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.order_pay_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderPayServiceApplication.class, args);
+	}
+
+}
