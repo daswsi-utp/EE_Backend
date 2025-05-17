74bcf0191ec2426086faf517e3979735fe1f56bf
roke741 <jhordieroque741@gmail.com>
2025-05-17 00:51:03 -0500

    chore: translate

--- report-service/src/main/java/com/report_service/controller/ReporteServiceRestController.java
+++ report-service/src/main/java/com/report_service/controller/ReporteServiceRestController.java
@@ -1,6 +1,5 @@
 package com.report_service.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -8,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/reportes")
+@RequestMapping("/api/report")
 public class ReporteServiceRestController {
 }
 
