f0adcf811e70f27d91bc229881157489972dcb78
roke741 <jhordieroque741@gmail.com>
2025-05-16 23:57:44 -0500

    feat: add rest controller

---
+++ report-service/src/main/java/com/report_service/controller/ReporteServiceRestController.java
@@ -0,0 +1,14 @@
+package com.report_service.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/reportes")
+public class ReporteServiceRestController {
+}
+
