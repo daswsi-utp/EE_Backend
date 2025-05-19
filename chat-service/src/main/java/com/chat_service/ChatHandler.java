9eb5c65662f541590d7fdbc3161855f9e28b1146
Luis Alatta <newluisalattago@gmail.com>
2025-05-19 03:24:23 -0500

    created chat handler and add code on websocketconfig.java

--- chat-service/src/main/java/com/chat_service/ChatHandler.java
+++ chat-service/src/main/java/com/chat_service/ChatHandler.java
@@ -1,4 +1,16 @@
 package com.chat_service;
 
-public class ChatHandler {
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Service
+public class ChatHandler extends TextWebSocketHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChatHandler.class);
+
+    private final CopyOnWriteArrayList<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
 }
--- chat-service/src/main/java/com/chat_service/WebSocketConfig.java
+++ chat-service/src/main/java/com/chat_service/WebSocketConfig.java
@@ -1,12 +1,22 @@
 package com.chat_service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    private ChatHandler chatHandler;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(null, "/chat");
+        registry.addHandler(chatHandler, "/chat");
     }
 }
