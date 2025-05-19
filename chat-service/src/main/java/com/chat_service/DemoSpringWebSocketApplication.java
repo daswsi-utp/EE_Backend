4dc826fe1066905872d89fe2e61421d8f139f668
Luis Alatta <newluisalattago@gmail.com>
2025-05-19 03:44:55 -0500

    modified chat.html and demospringwebsocketapplication

--- chat-service/src/main/java/com/chat_service/DemoSpringWebSocketApplication.java
+++ chat-service/src/main/java/com/chat_service/DemoSpringWebSocketApplication.java
@@ -1,4 +1,10 @@
 package com.chat_service;
 
+import org.springframework.boot.SpringApplication;
+
 public class DemoSpringWebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringWebSocketApplication.class, args);
+    }
 }
