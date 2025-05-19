cf0fa068e48a7616afb3127012f848ee19da78c9
Luis Alatta <newluisalattago@gmail.com>
2025-05-19 03:02:40 -0500

    websocket config created methods

--- chat-service/src/main/java/com/chat_service/WebSocketConfig.java
+++ chat-service/src/main/java/com/chat_service/WebSocketConfig.java
@@ -1,4 +1,12 @@
 package com.chat_service;
 
-public class WebSocketConfig {
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(null, "/chat");
+    }
 }
