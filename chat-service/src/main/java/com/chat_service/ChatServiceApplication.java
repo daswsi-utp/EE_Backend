39e24d3d6511837291849338770c89afc4d4df67
Luis Alatta <newluisalattago@gmail.com>
2025-05-20 03:38:25 -0500

    ChatServiceApplication.java modified

--- chat-service/src/main/java/com/chat_service/ChatServiceApplication.java
+++ chat-service/src/main/java/com/chat_service/ChatServiceApplication.java
@@ -1,4 +1,12 @@
 package com.chat_service;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ChatServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatServiceApplication.class, args);
+    }
 }
