fe8908b28966075831f6875fdeb3f907d54118b6
Luis Alatta <newluisalattago@gmail.com>
2025-05-20 03:42:17 -0500

    modified chat controller

--- chat-service/src/main/java/com/chat_service/controller/ChatController.java
+++ chat-service/src/main/java/com/chat_service/controller/ChatController.java
@@ -1,4 +1,24 @@
 package com.chat_service.controller;
 
+
+import com.chat_service.*;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(ChatMessage message) {
+        return message;
+    }
+
+    @MessageMapping("/chat.newUser")
+    @SendTo("/topic/public")
+    public ChatMessage newUser(ChatMessage message) {
+        message.setContent(message.getSender() + " se ha unido al chat");
+        return message;
+    }
 }
