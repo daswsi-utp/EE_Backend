ce09867aed55fc72df753fe787b2220a085a6bb4
Luis Alatta <newluisalattago@gmail.com>
2025-05-22 22:08:42 -0500

    fix

--- chat-service/src/main/java/com/chat_service/ChatServiceApplication.java
+++ chat-service/src/main/java/com/chat_service/ChatServiceApplication.java
@@ -4,11 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
 
 public class ChatServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChatServiceApplication.class, args);
     }
 }
+
+
+
