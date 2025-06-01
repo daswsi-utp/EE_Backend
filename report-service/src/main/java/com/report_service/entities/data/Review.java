49cde442545be379f2de7d9d852a7bfa36c607f1
roke741 <jhordieroque741@gmail.com>
2025-06-01 01:10:26 -0500

    refactor: reorganize Review class structure and methods for clarity

--- report-service/src/main/java/com/report_service/entities/data/Review.java
+++ report-service/src/main/java/com/report_service/entities/data/Review.java
@@ -6,4 +6,56 @@ public class Review {
     public String date;
     public String comment;
     public int helpful;
+
+
+    public Review() {
+    }
+
+    public Review(String user, int rating, String date, String comment, int helpful) {
+        this.user = user;
+        this.rating = rating;
+        this.date = date;
+        this.comment = comment;
+        this.helpful = helpful;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getHelpful() {
+        return helpful;
+    }
+
+    public void setHelpful(int helpful) {
+        this.helpful = helpful;
+    }
 }
