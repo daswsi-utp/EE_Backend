d0b0f49a18e376e20012d2a043af1f57fa4c8e03
Luis Alatta <newluisalattago@gmail.com>
2025-06-01 18:48:57 -0500

    add tiporeunion.java

--- event-service/src/main/java/com/event_service/entity/TipoReunion.java
+++ event-service/src/main/java/com/event_service/entity/TipoReunion.java
@@ -1,4 +1,16 @@
 package com.event_service.entity;
 
-public class TipoReunion {
+public enum TipoReunion {
+    CLIENTE("Cliente"),
+    EMPLEADO("Empleado");
+
+    private final String descripcion;
+
+    TipoReunion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
 }
