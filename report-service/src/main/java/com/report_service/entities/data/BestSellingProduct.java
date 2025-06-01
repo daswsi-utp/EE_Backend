72427a19c2342f4ad33a00426b645aeb40c2353b
roke741 <jhordieroque741@gmail.com>
2025-06-01 01:10:10 -0500

    fix: change value and revenue types in BestSellingProduct from int to double

--- report-service/src/main/java/com/report_service/entities/data/BestSellingProduct.java
+++ report-service/src/main/java/com/report_service/entities/data/BestSellingProduct.java
@@ -2,6 +2,39 @@ package com.report_service.entities.data;
 
 public class BestSellingProduct {
     public String name;
-    public int value;
-    public int revenue;
+    public double value;
+    public double revenue;
+
+    public BestSellingProduct() {
+    }
+
+    public BestSellingProduct(String name, double revenue, double value) {
+        this.name = name;
+        this.revenue = revenue;
+        this.value = value;
+    }
+
+    public double getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(double revenue) {
+        this.revenue = revenue;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
