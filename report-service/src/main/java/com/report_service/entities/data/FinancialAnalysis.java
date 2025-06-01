fdee0eb5f132211a84a48667482a2c3cdcda0d3c
roke741 <jhordieroque741@gmail.com>
2025-06-01 01:10:15 -0500

    fix: change value type in FinancialAnalysis from int to double

--- report-service/src/main/java/com/report_service/entities/data/FinancialAnalysis.java
+++ report-service/src/main/java/com/report_service/entities/data/FinancialAnalysis.java
@@ -3,5 +3,29 @@ package com.report_service.entities.data;
 public class FinancialAnalysis {
 
     public String name;
-    public int value;
+    public double value;
+
+    public FinancialAnalysis() {
+    }
+
+    public FinancialAnalysis(String name, double value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
 }
