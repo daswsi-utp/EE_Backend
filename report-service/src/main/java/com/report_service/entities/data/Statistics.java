a242347cf2fdbe5f1a98023054520b80d519264a
roke741 <jhordieroque741@gmail.com>
2025-06-01 00:27:16 -0500

    feat: add Statistics class to represent statistical data and its attributes

---
+++ report-service/src/main/java/com/report_service/entities/data/Statistics.java
@@ -0,0 +1,9 @@
+package com.report_service.entities.data;
+
+public class Statistics {
+    public String title;
+    public String value;
+    public String change;
+    public String icon;
+    public boolean positive;
+}
