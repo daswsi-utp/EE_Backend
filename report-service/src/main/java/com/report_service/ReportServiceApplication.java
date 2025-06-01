55b641d1179e84c8a0f60e01b1b4df05eb37f2af
roke741 <jhordieroque741@gmail.com>
2025-06-01 00:26:22 -0500

    refactor: remove unused modules.xml and commented EnableJpaRepositories

--- report-service/src/main/java/com/report_service/ReportServiceApplication.java
+++ report-service/src/main/java/com/report_service/ReportServiceApplication.java
@@ -3,9 +3,11 @@ package com.report_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+//@EnableJpaRepositories(basePackages = "com.report_service.repository")
 public class ReportServiceApplication {
 
 	public static void main(String[] args) {
