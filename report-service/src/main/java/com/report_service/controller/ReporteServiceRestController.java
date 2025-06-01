0b46dde67886cc53b4681ef53b21c16f882ff0b2
roke741 <jhordieroque741@gmail.com>
2025-06-01 10:31:30 -0500

    feat: Enhance product stock reporting with new endpoints and PDF/Excel exports

--- report-service/src/main/java/com/report_service/controller/ReporteServiceRestController.java
+++ report-service/src/main/java/com/report_service/controller/ReporteServiceRestController.java
@@ -6,6 +6,8 @@ import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.report_service.entities.Sale;
+import com.report_service.entities.data.Product;
+import com.report_service.entities.data.ProductAnalysis;
 import com.report_service.service.ReportService;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -40,11 +43,30 @@ public class ReporteServiceRestController {
         ReportService reportService = new ReportService();
         return ResponseEntity.ok(reportService.getFinancialAnalysis());
     }
-    @GetMapping("/sales/statistics")
+    @GetMapping("/statistics")
     public ResponseEntity<List<?>> getStatistics() {
         ReportService reportService = new ReportService();
         return ResponseEntity.ok(reportService.getStatistics());
     }
+
+    @GetMapping("/performance/channels")
+    public ResponseEntity<List<?>> getPerformanceByChannels() {
+        ReportService reportService = new ReportService();
+        return ResponseEntity.ok(reportService.getPerformanceByChannels());
+    }
+
+    @GetMapping("/financial-projections")
+    public ResponseEntity<List<?>> getFinancialProjections() {
+        ReportService reportService = new ReportService();
+        return ResponseEntity.ok(reportService.getFinancialProjections());
+    }
+
+    @GetMapping("/financial-indicators")
+    public ResponseEntity<List<?>> getFinancialIndicators() {
+        ReportService reportService = new ReportService();
+        return ResponseEntity.ok(reportService.getFinancialIndicators());
+    }
+
 
     @GetMapping("/products")
     public ResponseEntity<List<?>> getProducts() {
@@ -53,63 +75,83 @@ public class ReporteServiceRestController {
     }
 
 
-
-
-
-    @GetMapping("/sales/pdf")
-    public ResponseEntity<byte[]> getVentasPdf() throws IOException, DocumentException {
+    @GetMapping("/products/analysis")
+    public ResponseEntity<List<?>> getProductsAnalysis() {
         ReportService reportService = new ReportService();
-        List<Sale> sales = reportService.getSales();
+        List<ProductAnalysis> productAnalysisList = reportService.getProductAnalysis();
+        return ResponseEntity.ok(productAnalysisList);
+    }
+
+
+    @GetMapping("/products/stock")
+    public ResponseEntity<List<?>> getProductsStock() {
+        ReportService reportService = new ReportService();
+        List<Product> products = reportService.getProducts();
+
+        return ResponseEntity.ok(
+                products.stream().map(product -> new Object(){
+                    public final String category = product.getCategory();
+                    public final String name = product.getName();
+                    public final double price = product.getPrice();
+                    public final int stock = product.getStock();
+                }).toList()
+        );
+    }
+
+
+
+    @GetMapping("/products/stock/pdf")
+    public ResponseEntity<byte[]> getSockProductsPDF() throws IOException, DocumentException {
+        ReportService reportService = new ReportService();
+        List<Product> products = reportService.getProducts();
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         Document document = new Document();
         PdfWriter.getInstance(document, byteArrayOutputStream);
         document.open();
-        document.add(new Paragraph("Reporte de Ventas"));
+        document.add(new Paragraph("Reporte stock de productos"));
+        document.add(new Paragraph("\n"));
         document.add(new Paragraph("Fecha: " + LocalDate.now()));
-        PdfPTable table = new PdfPTable(5);
-        table.addCell("Fecha");
+        document.add(new Paragraph("\n"));
+        PdfPTable table = new PdfPTable(4);
+        table.addCell("Categoria");
         table.addCell("Producto");
-        table.addCell("Cantidad");
         table.addCell("Precio");
-        table.addCell("Total");
-        for (Sale sale : sales) {
-            table.addCell(sale.getDate().toString());
-            table.addCell(sale.getProduct().getName());
-            table.addCell(String.valueOf(sale.getQuantity()));
-            table.addCell(String.valueOf(sale.getProduct().getPrice()));
-            table.addCell(String.valueOf(sale.getTotal()));
+        table.addCell("Cantidad");
+        for (Product product : products) {
+            table.addCell(String.valueOf(product.getCategory()));
+            table.addCell(product.getName());
+            table.addCell(String.valueOf(product.getPrice()));
+            table.addCell(String.valueOf(product.getStock()));
         }
         document.add(table);
         document.close();
         return ResponseEntity.ok().contentType(MediaType.APPLICATION_PDF).body(byteArrayOutputStream.toByteArray());
     }
 
-    @GetMapping("/sales/excel")
-    public ResponseEntity<byte[]> getVentasExcel() throws IOException {
+    @GetMapping("/products/stock/excel")
+    public ResponseEntity<byte[]> getSockProductsExcel() throws IOException {
         ReportService reportService = new ReportService();
 
-        List<Sale> sales = reportService.getSales();
+        List<Product> products = reportService.getProducts();
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         XSSFWorkbook workbook = new XSSFWorkbook();
-        XSSFSheet sheet = workbook.createSheet("Ventas");
+        XSSFSheet sheet = workbook.createSheet("Stock");
         XSSFRow row = sheet.createRow(0);
-        row.createCell(0).setCellValue("Fecha");
+        row.createCell(0).setCellValue("Categoria");
         row.createCell(1).setCellValue("Producto");
-        row.createCell(2).setCellValue("Cantidad");
-        row.createCell(3).setCellValue("Precio");
-        row.createCell(4).setCellValue("Total");
+        row.createCell(2).setCellValue("Precio");
+        row.createCell(3).setCellValue("Cantidad");
         int rowIndex = 1;
-        for (Sale sale : sales) {
+        for (Product product : products) {
             row = sheet.createRow(rowIndex);
-            row.createCell(0).setCellValue(sale.getDate().toString());
-            row.createCell(1).setCellValue(sale.getProduct().getName());
-            row.createCell(2).setCellValue(String.valueOf(sale.getQuantity()));
-            row.createCell(3).setCellValue(String.valueOf(sale.getProduct().getPrice()));
-            row.createCell(4).setCellValue(String.valueOf(sale.getTotal()));
+            row.createCell(0).setCellValue(String.valueOf(product.getCategory()));
+            row.createCell(1).setCellValue(product.getName());
+            row.createCell(2).setCellValue(String.valueOf(product.getPrice()));
+            row.createCell(3).setCellValue(String.valueOf(product.getStock()));
             rowIndex++;
         }
         workbook.write(byteArrayOutputStream);
-        return ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM).header("Content-Disposition", "attachment; filename=ventas.xlsx").body(byteArrayOutputStream.toByteArray());
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM).header("Content-Disposition", "attachment; filename=stock.xlsx").body(byteArrayOutputStream.toByteArray());
     }
 
 
