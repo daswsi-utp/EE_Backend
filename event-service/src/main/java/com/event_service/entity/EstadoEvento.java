d579503d42c369e2491235ce406a3c38fae3e8af
Luis Alatta <newluisalattago@gmail.com>
2025-06-01 18:47:50 -0500

    edatdoevento . java

--- event-service/src/main/java/com/event_service/entity/EstadoEvento.java
+++ event-service/src/main/java/com/event_service/entity/EstadoEvento.java
@@ -1,4 +1,19 @@
 package com.event_service.entity;
 
-public class EstadoEvento {
+public enum EstadoEvento {
+    PROGRAMADO("Programado"),
+    EN_PROGRESO("En Progreso"),
+    COMPLETADO("Completado"),
+    CANCELADO("Cancelado"),
+    REPROGRAMADO("Reprogramado");
+
+    private final String descripcion;
+
+    EstadoEvento(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
 }
