3c17a30a8a58a791145fd2ef2d8621e9091822a5
roke741 <jhordieroque741@gmail.com>
2025-06-01 00:27:06 -0500

    feat: add Review class to represent user reviews and their attributes

---
+++ report-service/src/main/java/com/report_service/entities/data/Review.java
@@ -0,0 +1,9 @@
+package com.report_service.entities.data;
+
+public class Review {
+    public String user;
+    public int rating;
+    public String date;
+    public String comment;
+    public int helpful;
+}
