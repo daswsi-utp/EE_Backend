aa8d3e0173038ebbd0db12b27d32a3e0807dd272
Rodrigo Martinez <rodrigo.martinez@tecsup.edu.pe>
2025-06-02 02:20:31 -0500

    filter: employees by status or rol

--- employee-service/src/main/java/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
