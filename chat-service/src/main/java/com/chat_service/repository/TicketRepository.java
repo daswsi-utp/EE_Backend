714b155a95c2c324a5f6332e7186a15e6567e017
Marlon Diego <105674967+MarlonLupaca@users.noreply.github.com>
2025-07-02 01:49:05 -0500

    Update TicketRepository to filter active tickets and ownership

--- chat-service/src/main/java/com/chat_service/repository/TicketRepository.java
+++ chat-service/src/main/java/com/chat_service/repository/TicketRepository.java
@@ -1,4 +1,10 @@
 package com.chat_service.repository;
 
-public class TicketRepository {
+import com.chat_service.entity.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TicketRepository extends JpaRepository<Ticket, String> {
+    List<Ticket> findByStatus(String status);
 }
