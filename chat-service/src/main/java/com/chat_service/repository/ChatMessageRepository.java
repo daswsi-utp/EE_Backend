88e50edfc76fe45e684c74d0b14c4c9974e3f0ab
Marlon Diego <105674967+MarlonLupaca@users.noreply.github.com>
2025-07-02 01:49:05 -0500

    Enhance ChatMessageRepository with custom query support

--- chat-service/src/main/java/com/chat_service/repository/ChatMessageRepository.java
+++ chat-service/src/main/java/com/chat_service/repository/ChatMessageRepository.java
@@ -1,4 +1,11 @@
 package com.chat_service.repository;
 
-public interface ChatMessageRepository {
+import com.chat_service.entity.ChatMessageEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ChatMessageRepository extends JpaRepository<ChatMessageEntity, Long> {
+    List<ChatMessageEntity> findByTicket_Id(String ticketId);
+
 }
