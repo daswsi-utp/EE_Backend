393b65eec76956564bd6d936d3066da1d60ac7ca
Marlon Diego <105674967+MarlonLupaca@users.noreply.github.com>
2025-07-02 01:49:07 -0500

    Refactor InvoiceDetail to align with new invoice structure

--- order-pay-service/src/main/java/com/order_pay_service/models/InvoiceDetail.java
+++ order-pay-service/src/main/java/com/order_pay_service/models/InvoiceDetail.java
@@ -1,4 +1,35 @@
 package com.order_pay_service.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.order_pay_service.modelsAcopl.Product;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "invoice_details")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class InvoiceDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantity;
+
+    private Double unitPrice;
+
+    private Double subtotal;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "invoice_id")
+    @JsonBackReference
+    private Invoice invoice;
 }
+
