f285b0728f1721c1b0c88d1c838fd5a12445c2e8
Marlon Diego <105674967+MarlonLupaca@users.noreply.github.com>
2025-07-02 01:49:07 -0500

    Update InvoiceRequestDTO with extended request metadata

--- order-pay-service/src/main/java/com/order_pay_service/dto/InvoiceRequestDTO.java
+++ order-pay-service/src/main/java/com/order_pay_service/dto/InvoiceRequestDTO.java
@@ -1,4 +1,14 @@
 package com.order_pay_service.dto;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
 public class InvoiceRequestDTO {
+    private String customerCode;
+    private String shippingAddress;
+    private Double totalAmount;
+    private List<InvoiceDetailDTO> details;
 }
