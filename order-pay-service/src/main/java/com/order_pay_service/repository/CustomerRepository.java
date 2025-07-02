3a156579e162136e16dfa9ff5bd80bba0affbd80
Marlon Diego <105674967+MarlonLupaca@users.noreply.github.com>
2025-07-02 01:49:08 -0500

    Update CustomerRepository with custom lookup method

--- order-pay-service/src/main/java/com/order_pay_service/repository/CustomerRepository.java
+++ order-pay-service/src/main/java/com/order_pay_service/repository/CustomerRepository.java
@@ -1,4 +1,10 @@
 package com.order_pay_service.repository;
 
-public class CustomerRepository {
+import com.order_pay_service.modelsAcopl.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<com.order_pay_service.modelsAcopl.Customer, Long> {
+    Customer findByUserCode(String userCode);
 }
