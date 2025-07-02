92507475e4d099b5ceaf9274bb27b05daa2ca67f
Marlon Diego <105674967+MarlonLupaca@users.noreply.github.com>
2025-07-02 01:49:09 -0500

    Update ProductRepository with stock check queries

--- order-pay-service/src/main/java/com/order_pay_service/repository/ProductRepository.java
+++ order-pay-service/src/main/java/com/order_pay_service/repository/ProductRepository.java
@@ -1,4 +1,10 @@
 package com.order_pay_service.repository;
 
-public interface ProductRepository {
+import com.order_pay_service.modelsAcopl.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Product findByCode(Integer code); // Optional if you're using code as business identifier
 }
