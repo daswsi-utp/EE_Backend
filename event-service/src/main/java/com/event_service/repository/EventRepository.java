7b8c6f4e37dad46098b6bcc4508f395b66dc4a97
Marlon Diego <105674967+MarlonLupaca@users.noreply.github.com>
2025-07-03 15:35:28 -0500

    Feat: add EventRepository interface for event persistence operations

--- event-service/src/main/java/com/event_service/repository/EventRepository.java
+++ event-service/src/main/java/com/event_service/repository/EventRepository.java
@@ -1,4 +1,6 @@
 package com.event_service.repository;
+import com.event_service.models.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EventRepository {
+public interface EventRepository extends JpaRepository<Event, String> {
 }
