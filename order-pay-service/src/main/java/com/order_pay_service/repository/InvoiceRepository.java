de7a0eb8b0af47cefe2ebd60ca45a42752e65843
Marlon Diego <105674967+MarlonLupaca@users.noreply.github.com>
2025-07-03 01:44:20 -0500

    refactor(invoice-repo): adjust query methods

--- order-pay-service/src/main/java/com/order_pay_service/repository/InvoiceRepository.java
+++ order-pay-service/src/main/java/com/order_pay_service/repository/InvoiceRepository.java
@@ -4,7 +4,10 @@ import com.order_pay_service.models.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, String> {
+    List<Invoice> findByCustomerUserCode(String userCode);
     boolean existsBySeriesNumber(String seriesNumber);
 }
